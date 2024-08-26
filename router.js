a1092b1be7bcfc02fb4234ac08f68b52bfde0338
wlqgkrry <wlqgkrry@gmail.com>
2024-08-27 01:10:47 +0900

    shopping mall project complete

---
+++ router.js
@@ -0,0 +1,17 @@
+function route(pathname, handle, response, productId){
+    console.log('pathname : ' + pathname);
+
+    if(typeof handle[pathname] == "function"){
+        handle[pathname](response, productId);
+    }
+    else{
+        response.writeHead(404, {'Content-Type' : 'text/html'});
+        response.write('Not Found.');
+        response.end();
+    }
+
+}
+
+exports.route = route;
+
+
